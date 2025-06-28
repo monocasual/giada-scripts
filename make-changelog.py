5b06368ed9de66a460e48d5466e2fd3948223170
Giovanni Zuliani <monocasual@gmail.com>
2025-06-28 15:11:46 +0200

    Add make-changelog.py script

---
+++ make-changelog.py
@@ -0,0 +1,71 @@
+import xml.etree.ElementTree as et
+import argparse
+import sys
+import os
+
+
+SOURCE_PATH = "../giada/extras/com.giadamusic.Giada.metainfo.xml"
+CHANGELOG_APP_PATH = "../giada/ChangeLog"
+CHANGELOG_WEB_PATH = "../giada-www/src/data/changes.html"
+
+
+def get_changes_from_xml(file_path, version):
+    check_file_existence(file_path)
+    root = et.parse(file_path).getroot()
+    changes = root.findall(f".//release[@version='{version}']/description/ul/li")
+    if changes == []:
+        print(f"Could not find release {version} in {file_path}!")
+        sys.exit(1)
+    return [li.text.strip() for li in changes]
+
+
+def check_file_existence(file_path):
+    if not os.path.exists(file_path):
+        print(f"File {file_path} doesn't exist!")
+        sys.exit(1)
+
+
+def update_app_changelog(file_path, version, date, changes):
+    check_file_existence(file_path)
+    with open(file_path, "r") as f:
+        content = f.read()
+        if f"{version} ---" in content:
+            print(
+                f"App changelog already compiled for version {version}, nothing to do"
+            )
+            return
+        changes_txt = f"{version} --- {date}\n"
+        for change in changes:
+            changes_txt += f"- {change}\n"
+        changes_txt += "\n\n"
+    with open(file_path, "w") as f:
+        f.write(changes_txt + content)
+
+
+def update_web_changelog(file_path, changes):
+    check_file_existence(file_path)
+    with open(file_path, "w") as f:
+        content = "<ul>\n"
+        for change in changes:
+            content += f"\t<li>{change}</li>\n"
+        content += "</ul>"
+        f.write(content)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Changelog generator")
+
+    parser.add_argument("--version", help="Version number, e.g. 1.0.0", required=True)
+
+    parser.add_argument(
+        "--date", help="Release date, e.g. January 5, 1990", required=True
+    )
+
+    args = vars(parser.parse_args())
+    version = args["version"]
+    date = args["date"]
+
+    changes = get_changes_from_xml(SOURCE_PATH, version)
+
+    update_app_changelog(CHANGELOG_APP_PATH, version, date, changes)
+    update_web_changelog(CHANGELOG_WEB_PATH, changes)
